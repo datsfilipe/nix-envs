bb4fde76a8cf188d3ace0ba10f35e6e52ddba685
Filipe Lima <datsfilipe.foss@proton.me>
2025-02-16 11:22:42 -0300

    feat: add development environment templates and main flake

---
+++ README.md
@@ -0,0 +1,5 @@
+# nix-envs
+
+Nix powered development environment templates
+
+Use `nix flake new -t github:datsfilipe/nix-envs#language` to use the desired template
---
+++ flake.nix
@@ -0,0 +1,87 @@
+{
+  description = "Nix powered development environment templates";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: rec {
+    templates = {
+      bun = {
+        path = ./bun;
+        description = "Bun template";
+      };
+
+      python = {
+        path = ./nodejs;
+        description = "NodeJS template";
+      };
+
+      go = {
+        path = ./go;
+        description = "Golang template";
+      };
+
+      golang = templates.go;
+
+      crystal = {
+        path = ./crystal;
+        description = "Crystal template";
+      };
+
+      rust = {
+        path = ./rust;
+        description = "Rust template";
+      };
+
+      electron = {
+        path = ./electron;
+        description = "Electron template";
+      };
+
+      elixir = {
+        path = ./elixir;
+        description = "Elixir template";
+      };
+
+      qmk = {
+        path = ./qmk;
+        description = "QMK template";
+      };
+
+      prisma = {
+        path = ./prisma;
+        description = "Prisma template";
+      };
+
+      git-hooks = {
+        path = ./git-hooks;
+        description = "Git hooks template";
+      };
+
+      work = {
+        path = ./work;
+        description = "Work template";
+      };
+    };
+
+    devShells.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          alejandra
+        ];
+        name = "nix-envs";
+      };
+    };
+
+    formatter = let
+      system = "x86_64-linux";
+    in
+      nixpkgs.legacyPackages.${system}.alejandra;
+  };
+}
